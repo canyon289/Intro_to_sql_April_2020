6c43911201deff6e8c7bc2b8a949df7e2151b51c
Ravin Kumar <ravinsdrive@gmail.com>
2020-04-12 13:45:41 -0700

    Update setup

--- SQL102/setup.sql
+++ SQL102/setup.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS table_1;
 CREATE TABLE table_1 (
 	table_1_id INTEGER PRIMARY KEY,
 	letter TEXT NOT NULL
@@ -8,7 +9,7 @@ VALUES (1, "a" ),
 (2, "b" ),
 (3, "c" );
 
-
+DROP TABLE IF EXISTS table_2;
 CREATE TABLE table_2 (
 	table_2_key INTEGER PRIMARY KEY,
 	letter TEXT NOT NULL
@@ -18,8 +19,6 @@ INSERT INTO table_2 (table_2_key, letter)
 VALUES (2, "b" ),
 (3, "c" ),
 (4, "d" );
-
-
 
 delete from "order"
 where employeeid in (4,7)
